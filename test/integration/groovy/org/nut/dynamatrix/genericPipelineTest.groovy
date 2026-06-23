fe4434849929b7091292c511c7c9bd6d98405757
Jim Klimov <jimklimov+nut@gmail.com>
2026-06-23 11:48:50 +0200

    genericPipelineTest: add also a test case to list and count visible plugins
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- test/integration/groovy/org/nut/dynamatrix/genericPipelineTest.groovy
+++ test/integration/groovy/org/nut/dynamatrix/genericPipelineTest.groovy
@@ -44,6 +44,38 @@ class genericPipelineTest {
         rule.assertLogContains('The build number is even', secondResult)
     }
 
+    /** Half test, half JSL/test developer troubleshooting aid */
+    @Test
+    void "Testing list of loaded plugins"() {
+        final CpsFlowDefinition flow = new CpsFlowDefinition('''
+			import jenkins.model.Jenkins
+            def plugins = jenkins.model.Jenkins.instance.getPluginManager().getPlugins()
+            echo "=== The following ${plugins?.size()} plugins are installed:"
+            String l = ""
+			plugins.each {
+				l += "${it.getDisplayName()} (${it.getShortName()}): ${it.getVersion()}\\n"
+			}
+			echo "${l}"
+            echo "=== End of list"
+        ''', false)
+
+        final WorkflowJob workflowJob = rule.createProject(WorkflowJob, 'p')
+        workflowJob.definition = flow
+        WorkflowRun run = workflowJob.scheduleBuild2(0).get()
+
+        rule.assertBuildStatus(hudson.model.Result.SUCCESS, run)
+
+        def plugins = rule.getInstance().pluginManager.plugins
+        System.err.println("=== For reference, JenkinsRule plugin list (${plugins?.size()}) reported by test harness:".toString())
+        String l = ""
+        plugins?.each {
+            l += "${it.getDisplayName()} (${it.getShortName()}): ${it.getVersion()}\n"
+        }
+        System.err.println(l + "\n=== End of list")
+
+        rule.assertLogContains("durable-task", run)
+    }
+
     /** Make sure gradle setup lets us run all the integration tests
      * (some fail without prerequisites below, solutions to which
      * are on the recipe's side rather than JSL or test code base).
